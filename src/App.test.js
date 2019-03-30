25138ee635d2fad02c43cd88d4343ea93935ea8c
Turzhanskyi <turzhansky81@gmail.com>
2019-03-30 23:01:58 +0200

    Step 1: Install React.js

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div class="container">
+          React Axios Tutorial
+        </div>
+    );
+  }
+}
+
+export default App;
